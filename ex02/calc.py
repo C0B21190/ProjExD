d6c4815be9dcc18facb599f8539f165609f9143a
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-06-21 15:35:31 +0900

    lll

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,33 +2,19 @@ from distutils.command.build import build
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
-# root = tk.Tk()
-# #root.title("おためしか")
-# root.geometry('300x500')
-
-# # label = tk.Label(root,
-# #                 text='ラベルです',
-# #                 font=('Ricty Diminished',20)
-# #                 )
-# # label.pack()
-# def button_click(event):
-#     btn = event.widget
-#     txt = btn['text']
-#     tkm.showwarning('警告',f'[{txt}]押しました')
-
-# button = tk.Button(root,text='押すな',command=button_click)
-
-
-# button.bind('<1>',button_click)
-# button.pack()
-
-# root.mainloop()
 def button_click(event):
     btn = event.widget
     num = btn['text']
-    #tkm.showinfo('',f'{num}押しました') #警告メッセージ
-    entry.insert(tk.END,f'{num}')
+    if num == '=':
+        eqn = entry.get()
+        res = eval(eqn)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,res)
+
+    else:
+        #tkm.showinfo('',f'{num}押しました') #警告メッセージ
+        entry.insert(tk.END,num')
+
 
 
 if __name__ == '__main__':
@@ -41,7 +27,8 @@ if __name__ == '__main__':
     
 
     r,c = 1,0
-    for i,num in enumerate([i for i in range(9,-1,-1)]+['+']):
+    #l=['9','8','7','6','5','4','3','2','1','0','+','=']
+    for i,num in enumerate([9,8,7,6,5,4,3,2,1,"+",'=']):
         btn = tk.Button(root,
                         text=f'{num}',
                         width=4,
@@ -51,10 +38,10 @@ if __name__ == '__main__':
         btn.bind('<1>',button_click)
         btn.grid(row=r,column=c)
         c += 1
+        #d=l[num]
         if (i+1)%3 == 0:
             r+=1
             c=0
-    
         
     root.mainloop()
 
