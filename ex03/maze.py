c6a9eeba25e4dce0461a46c69234c2979b8ec48d
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-06-28 14:38:50 +0900

    問題6完成

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,6 +5,10 @@ def key_down(event):
     key = event.keysym
     print(f'{key}woshita')
 
+def key_up(event):
+    global key
+    key = ''
+
 if __name__ == "__main__":
     root = tk.Tk('迷えるこうかとん')
     canvas = tk.Canvas(root,
@@ -18,4 +22,5 @@ if __name__ == "__main__":
 
     key = ''
     root.bind('<KeyPress>',key_down)
+    root.bind('<KeyRelease>',key_up)
     root.mainloop()
