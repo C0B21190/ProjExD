ae52c9d57df8676545e3463a4e4e125535c7de67
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-06-28 14:36:06 +0900

    問題4&5完成

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,15 +1,21 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+    print(f'{key}woshita')
 
 if __name__ == "__main__":
     root = tk.Tk('迷えるこうかとん')
     canvas = tk.Canvas(root,
-            width=1500,
-            height=900,
+            width=500,
+            height=600,
             bg='black' )
     canvas.pack()
     tori = tk.PhotoImage(file="ex03/fig/5.png")
     cx ,cy = 300, 400
     canvas.create_image(cx, cy, image=tori, tag='tori')
 
+    key = ''
+    root.bind('<KeyPress>',key_down)
     root.mainloop()
