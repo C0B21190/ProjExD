d17b034266344ae78e7e3f106490575deb060b4b
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-07-05 15:48:49 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,4 +1,5 @@
 import random
+import re
 import pygame as pg
 import sys
 
@@ -26,9 +27,25 @@ def main():
     bmimg_rct = bmimg_sfc.get_rect()
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
     bmimg_rct.centery = random.randint(0, screen_rct.height)
+
+    # bmimg_sfc2 = pg.Surface((20, 20)) #surface
+    # bmimg_sfc2.set_colorkey((0, 0, 0))
+    # pg.draw.circle(bmimg_sfc2, (255, 0, 0), (10,10), 10)
+    # bmimg_rct2 = bmimg_sfc2.get_rect()
+    # bmimg_rct2.centerx = random.randint(0, screen_rct.width)
+    # bmimg_rct2.centery = random.randint(0, screen_rct.height)
+
+    # bmimg_sfc3 = pg.Surface((20, 20)) #surface
+    # bmimg_sfc3.set_colorkey((0, 0, 0))
+    # pg.draw.circle(bmimg_sfc3, (255, 0, 0), (10,10), 10)
+    # bmimg_rct3 = bmimg_sfc3.get_rect()
+    # bmimg_rct3.centerx = random.randint(0, screen_rct.width)
+    # bmimg_rct3.centery = random.randint(0, screen_rct.height)
     
     #6
     vx, vy = +1, +1
+    # vx2, vy2 = +1, +1
+    # vx3, vy3 = +1, +1
 
 
 
@@ -57,72 +74,39 @@ def main():
         
         #06
         bmimg_rct.move_ip(vx, vy)
+        # bmimg_rct2.move_ip(vx2, vy2)
+        # bmimg_rct3.move_ip(vx3, vy3)
 
         #05
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
+        # screen_sfc.blit(bmimg_sfc2,bmimg_rct2)
+        # screen_sfc.blit(bmimg_sfc3,bmimg_rct3)
 
         #07
         yoko, tate =  check_bound(bmimg_rct,screen_rct)
         vx *= yoko
         vy *= tate
 
+        # yoko, tate =  check_bound(bmimg_rct2,screen_rct)
+        # vx2 *= yoko
+        # vy2 *= tate
+
+        # yoko, tate =  check_bound(bmimg_rct3,screen_rct)
+        # vx3 *= yoko
+        # vy3 *= tate
+
+        #08
+        if kkimg_rct.colliderect(bmimg_rct): return
+
+
         pg.display.update()
         clock.tick(1000)
 #07
 def check_bound(rct, scr_rct):
-    #rcr: kokato or boom
-    #scr_rcr: screen rect
     yoko, tate = +1, +1
     if rct.left < scr_rct.left or scr_rct.right < rct.right: yoko = -1
     if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom: tate = -1
     return yoko, tate
-    
-
-
-
-
-    # clock = pg.time.Clock()
-
-    # pg.display.set_caption('初めてPygame')
-    # screen = pg.display.set_mode((800,600))
-    
-    # tori_img = pg.image.load('fig/3.png')
-    # tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
-    # tori_rect = tori_img.get_rect()
-    # tori_rect.center = 700, 400
-    # screen.blit(tori_img, tori_rect)
-
-    # pg.display.update()
-
-    # clock.tick(0.1)
-
-#     tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
-#     tori_rect = tori_img.get_rect()
-    
-
-
-    
-    
-    
-#     screen.blit(tori_img, tori_rect)
-#     fonto = pg.font.Font(None, 80)
-#     txt = fonto.render(str(tmr), True, WHITE)
-#     screen.blit(txt, (300, 200))
-
-
-# for event in pg.event.get():
-#     if event.type == pg.QUIT: return
-
-# if event.type == pg.KEYDOWN and event.key == pg.K_F1:
-#     screen = py.display.set_mode((800, 600), pg.FULLSCREEN)
-# if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-#     screen = py.display.set_mode((800, 600))
-
-# key_lst = pg.key.get_pressed()
-# print(key_lst[pg.K_SPACE])
-
-# print(pg.mouse.get_pos())
-
 
 if __name__ == "__main__":
     pg.init()
