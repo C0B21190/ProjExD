5656ae004b3abde8132b60def234a6630b3bd7f4
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-07-05 14:24:45 +0900

    練習問題3完成した

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,21 +4,31 @@ import sys
 def main():
     clock = pg.time.Clock()
 
+    #01
     pg.display.set_caption('dggdgdg')
-    screen_sfc = pg.display.set_mode((800, 450))
+    screen_sfc = pg.display.set_mode((1120, 630))
     screen_rct = screen_sfc.get_rect()
     bgimg_sfc = pg.image.load('fig/pg_bg.jpg')
     bgimg_rct = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc,bgimg_rct)
 
+    #03
+    kkimg_sfc = pg.image.load('fig/6.png')
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900, 400
+
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
 
         #02
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         pg.display.update()
         clock.tick(1000)
+
+
 
 
 
