ae5fbe1fb51f67109f51f332910469d29ea733f2
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-07-05 17:12:33 +0900

    4限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,7 @@ import random
 import re
 import pygame as pg
 import sys
+import tkinter.messagebox as tkm
 
 def main():
     clock = pg.time.Clock()
@@ -21,32 +22,35 @@ def main():
     kkimg_rct.center = 900, 400
 
     #05 boom
-    bmimg_sfc = pg.Surface((20, 20)) #surface
+    bmimg_sfc = pg.Surface((50, 50)) #surface
     bmimg_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10,10), 10)
+    pg.draw.circle(bmimg_sfc, (255, 0, 0), (25,25), 25)
     bmimg_rct = bmimg_sfc.get_rect()
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
     bmimg_rct.centery = random.randint(0, screen_rct.height)
 
-    # bmimg_sfc2 = pg.Surface((20, 20)) #surface
-    # bmimg_sfc2.set_colorkey((0, 0, 0))
-    # pg.draw.circle(bmimg_sfc2, (255, 0, 0), (10,10), 10)
-    # bmimg_rct2 = bmimg_sfc2.get_rect()
-    # bmimg_rct2.centerx = random.randint(0, screen_rct.width)
-    # bmimg_rct2.centery = random.randint(0, screen_rct.height)
+    bmimg_sfc2 = pg.Surface((50, 50)) #surface
+    bmimg_sfc2.set_colorkey((0, 0, 0))
+    pg.draw.circle(bmimg_sfc2, (255, 0, 0), (25,25), 25)
+    bmimg_rct2 = bmimg_sfc2.get_rect()
+    bmimg_rct2.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct2.centery = random.randint(0, screen_rct.height)
 
-    # bmimg_sfc3 = pg.Surface((20, 20)) #surface
-    # bmimg_sfc3.set_colorkey((0, 0, 0))
-    # pg.draw.circle(bmimg_sfc3, (255, 0, 0), (10,10), 10)
-    # bmimg_rct3 = bmimg_sfc3.get_rect()
-    # bmimg_rct3.centerx = random.randint(0, screen_rct.width)
-    # bmimg_rct3.centery = random.randint(0, screen_rct.height)
+    bmimg_sfc3 = pg.Surface((50, 50)) #surface
+    bmimg_sfc3.set_colorkey((0, 0, 0))
+    pg.draw.circle(bmimg_sfc3, (0, 0, 255), (25,25), 25)
+    bmimg_rct3 = bmimg_sfc3.get_rect()
+    bmimg_rct3.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct3.centery = random.randint(0, screen_rct.height)
     
     #6
     vx, vy = +1, +1
-    # vx2, vy2 = +1, +1
-    # vx3, vy3 = +1, +1
+    vx2, vy2 = +1, +2
+    vx3, vy3 = +2, +2
 
+    score = 0
+    font = pg.font.Font(None, 55)
+    # text = font.render(str(score),True, (255,255,255))
 
 
     while True:
@@ -56,6 +60,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
+
         #04
         key_states = pg.key.get_pressed() #jisyo
         if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
@@ -69,35 +74,47 @@ def main():
             if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
             if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
             if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
+ 
 
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
         
         #06
         bmimg_rct.move_ip(vx, vy)
-        # bmimg_rct2.move_ip(vx2, vy2)
-        # bmimg_rct3.move_ip(vx3, vy3)
+        bmimg_rct2.move_ip(vx2, vy2)
+        bmimg_rct3.move_ip(vx3, vy3)
 
         #05
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
-        # screen_sfc.blit(bmimg_sfc2,bmimg_rct2)
-        # screen_sfc.blit(bmimg_sfc3,bmimg_rct3)
+        screen_sfc.blit(bmimg_sfc2,bmimg_rct2)
+        screen_sfc.blit(bmimg_sfc3,bmimg_rct3)
 
         #07
         yoko, tate =  check_bound(bmimg_rct,screen_rct)
         vx *= yoko
         vy *= tate
 
-        # yoko, tate =  check_bound(bmimg_rct2,screen_rct)
-        # vx2 *= yoko
-        # vy2 *= tate
+        yoko, tate =  check_bound(bmimg_rct2,screen_rct)
+        vx2 *= yoko
+        vy2 *= tate
 
-        # yoko, tate =  check_bound(bmimg_rct3,screen_rct)
-        # vx3 *= yoko
-        # vy3 *= tate
+        yoko, tate =  check_bound(bmimg_rct3,screen_rct)
+        vx3 *= yoko
+        vy3 *= tate
 
         #08
-        if kkimg_rct.colliderect(bmimg_rct): return
-
+        if kkimg_rct.colliderect(bmimg_rct): 
+            score = score+1
+        if kkimg_rct.colliderect(bmimg_rct2): 
+            score = score+2
+        if kkimg_rct.colliderect(bmimg_rct3): 
+            score = score-3
+        
+        
+        text = font.render(str(score),True, (255,255,255))
+        screen_sfc.blit(text,[100,100])
+        if score < 0 :
+            tkm.showwarning('gameover','GAMEOVER!')
+            return
 
         pg.display.update()
         clock.tick(1000)
