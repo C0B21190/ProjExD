4abbc49375eb87531d73f06cf1e6f09972d1f658
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-07-05 14:15:46 +0900

    練習問題1&2完成した

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,18 +1,43 @@
 import pygame as pg
-
+import sys
 
 def main():
     clock = pg.time.Clock()
 
-    pg.display.set_caption('初めてPygame')
-    screen = pg.display.set_mode((800,600))
-    
-    tori_img = pg.img.load('ex03/fig/3.png')
-    tori_rect = tori_img.get_rect()
-    tori_rect.center = 700, 400
-    screen.blit(tori_img, tori_rect)
+    pg.display.set_caption('dggdgdg')
+    screen_sfc = pg.display.set_mode((800, 450))
+    screen_rct = screen_sfc.get_rect()
+    bgimg_sfc = pg.image.load('fig/pg_bg.jpg')
+    bgimg_rct = bgimg_sfc.get_rect()
+    screen_sfc.blit(bgimg_sfc,bgimg_rct)
 
-    clock.tick(0.2)
+    while True:
+        screen_sfc.blit(bgimg_sfc,bgimg_rct)
+
+        #02
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        pg.display.update()
+        clock.tick(1000)
+
+
+
+
+    # clock = pg.time.Clock()
+
+    # pg.display.set_caption('初めてPygame')
+    # screen = pg.display.set_mode((800,600))
+    
+    # tori_img = pg.image.load('fig/3.png')
+    # tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
+    # tori_rect = tori_img.get_rect()
+    # tori_rect.center = 700, 400
+    # screen.blit(tori_img, tori_rect)
+
+    # pg.display.update()
+
+    # clock.tick(0.1)
+
 #     tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
 #     tori_rect = tori_img.get_rect()
     
@@ -42,5 +67,8 @@ def main():
 
 
 if __name__ == "__main__":
-
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
 
