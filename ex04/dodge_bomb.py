20e1de2f0797c160aa67ee4a3a0b41e47e3d9166
C0B21190 <c0b2119037@edu.teu.ac.jp>
2022-07-05 15:33:15 +0900

    練習問題7完成した

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -45,6 +45,14 @@ def main():
         if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+
+        #07
+        if check_bound(kkimg_rct, screen_rct) != (1, 1):
+            if key_states[pg.K_UP]    == True: kkimg_rct.centery += 1
+            if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
+            if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
+            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
+
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
         
         #06
@@ -53,10 +61,22 @@ def main():
         #05
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
 
+        #07
+        yoko, tate =  check_bound(bmimg_rct,screen_rct)
+        vx *= yoko
+        vy *= tate
+
         pg.display.update()
         clock.tick(1000)
-
-
+#07
+def check_bound(rct, scr_rct):
+    #rcr: kokato or boom
+    #scr_rcr: screen rect
+    yoko, tate = +1, +1
+    if rct.left < scr_rct.left or scr_rct.right < rct.right: yoko = -1
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom: tate = -1
+    return yoko, tate
+    
 
 
 
